dcabe10bb3c7709610035af794cc8b3f2e45eb7e
Daniela Gonzalez Moreno <daniela.gonzalez.m02@mail.pucv.cl>
2021-08-13 01:20:21 -0400

    Update TMO.py

--- TMO.py
+++ TMO.py
@@ -39,4 +39,56 @@ for x in range(1,(num_rows+1)):
 	lista_all.append(lista_aux)
 	lista_aux=list()
 
-print(lista_all)
+print("Accediendo a: "+lista_all[0][1])
+driver.get(lista_all[0][1])
+
+print("Obteniendo cápitulos")
+elem = driver.find_element_by_id("show-chapters")
+elem.click()
+
+num_rows_1 = len (driver.find_elements_by_xpath("//*[@id='chapters']/ul/li"))
+print (num_rows_1)
+
+#for primera sección
+before_XPath_H="//*[@id='chapters']/ul/li["
+after_XPath_H="]/h4/div/div[2]/span"
+cap_sl=list()
+for x in range(1,(num_rows_1+1)):
+	h_text=before_XPath_H + str(x) + after_XPath_H
+	estado = driver.find_element_by_xpath(h_text).get_attribute('class')
+	elem = driver.find_element_by_xpath("//*[@id='chapters']/ul/li["+str(x)+"]/h4/div/div[1]/a")
+	if estado=="chapter-viewed-icon":
+		cap_sl.append(elem)
+		print("Capitulo sin leer")
+
+print(len(cap_sl))
+if(len(cap_sl)>9):
+	print("faltan cap")
+	num_rows_2 = len (driver.find_elements_by_xpath("//*[@id='chapters-collapsed']/li"))
+	print (num_rows_2)
+	before_XPath_H="//*[@id='chapters-collapsed']/li["
+	after_XPath_H="]/h4/div/div[2]/span"
+	cap_sl=list()
+
+	for x in range(1,(num_rows_2+1)):
+		h_text=before_XPath_H + str(x) + after_XPath_H
+		estado = driver.find_element_by_xpath(h_text).get_attribute('class')
+		elem = driver.find_element_by_xpath("//*[@id='chapters-collapsed']/li["+ str(x) +"]/h4/div/div[1]/a" )
+		if estado=="chapter-viewed-icon":
+			cap_sl.append(elem)
+			print("Capitulo sin leer")
+
+	elem = driver.find_element_by_xpath("//*[@id='chapters-collapsed']/li["+str(len(cap_sl))+"]/h4/div/div[1]/a")
+	elem.click()
+	elem = driver.find_element_by_xpath("//*[@id='chapters-collapsed']/li["+str(len(cap_sl))+"]/div/div/ul/li/div/div[6]/a")
+	elem.click()
+
+else:
+	elem = driver.find_element_by_xpath("//*[@id='chapters']/ul/li["+str(len(cap_sl))+"]/h4/div/div[1]/a")
+	elem.click()
+	elem = driver.find_element_by_xpath("//*[@id='chapters']/ul/li["+str(len(cap_sl))+"]/div/div/ul/li/div/div[6]/a")
+	elem.click()
+
+
+
+
